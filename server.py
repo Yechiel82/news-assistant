813e07ed73ea235f391c9221fa654ec7a968a4e3
Aleksandr Bobrov <al.bobrov.mail@gmail.com>
2023-12-22 18:46:13 +0400

    aider: Update server.py to work with app.py.
    # Aider chat conversation:
    
    USER: please change `server.py` to work with app.py
    
    ASSISTANT: To make `server.py` work with `app.py`, I will:
    
    1. Correct the import statement to import `ping` and `perform` from `app.app` instead of `.main`.
    2. Ensure that the `app` variable from `server.py` does not conflict with the `app` module.
    
    Here are the diffs for those changes:
    
    ```diff
    --- server.py
    +++ server.py
    @@ ... @@
    -from .main import ping, perform
    +from app.app import ping, perform
    
     app = FastAPI()
    
    @@ ... @@
    @app.get('/')
    async def read_root():
    -    return perform()
    +    return {"message": perform()}
    
    @app.get('/ping')
    async def read_ping():
    -    return ping()
    +    return {"message": ping()}
    ```
    
    These changes will update the import statements and wrap the return values in a dictionary to conform to FastAPI's expected response model.

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
-from .main import ping, perform
+from app.app import ping, perform
 
 app = FastAPI()
 
 @app.get('/')
 async def read_root():
-    return perform()
+    return {"message": perform()}
 
 @app.get('/ping')
 async def read_ping():
-    return ping()
+    return {"message": ping()}
