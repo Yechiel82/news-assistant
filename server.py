363f00635af497221e63947b362372069d9f08cd
Aleksandr Bobrov <al.bobrov.mail@gmail.com>
2023-12-22 18:48:45 +0400

    aider: Add code to start FastAPI server using Uvicorn when `server.py` is run directly.
    # Aider chat conversation:
    
    USER: please change `server.py`, when I run python server.py it just do nothing, it should listen port to allow requests
    
    ASSISTANT: To make `server.py` listen on a port, we need to add code to start the FastAPI server using Uvicorn when the script is run directly. Here's the diff for the changes:
    
    ```diff
    --- server.py
    +++ server.py
    @@ ... @@
     from fastapi import FastAPI
     from app.app import ping, perform
    
     app = FastAPI()
    
     @app.get('/')
     async def read_root():
         return {"message": perform()}
    
     @app.get('/ping')
     async def read_ping():
         return {"message": ping()}
    +
    +if __name__ == "__main__":
    +    import uvicorn
    +    uvicorn.run(app, host="0.0.0.0", port=8000)
    ```
    
    This change adds an `if __name__ == "__main__":` block to the end of `server.py`, which starts the Uvicorn server with the FastAPI app on port 8000 when the script is executed directly.

--- server.py
+++ server.py
@@ -10,3 +10,7 @@ async def read_root():
 @app.get('/ping')
 async def read_ping():
     return {"message": ping()}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
