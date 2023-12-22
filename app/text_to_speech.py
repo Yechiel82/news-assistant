7fb28f1a66def35b198bcfd9d63bb4aefd8bfa08
Aleksandr Bobrov <al.bobrov.mail@gmail.com>
2023-12-22 18:14:43 +0400

    aider: Refactored text_to_speech function to use OpenAI client and generate speech from input text.
    # Aider chat conversation:
    
    ASSISTANT: ```diff
    --- app/text_to_speech.py
    +++ app/text_to_speech.py
    @@ ... @@
    -from pathlib import Path
    -from openai import OpenAI
    -client = OpenAI()
    +from pathlib import Path
    +from openai import OpenAI
    +
    +def text_to_speech(text):
    +    client = OpenAI()
    +    speech_file_path = Path(__file__).parent / "speech.mp3"
    +    response = client.audio.speech.create(
    +        model="tts-1",
    +        voice="alloy",
    +        input=text
    +    )
    +    response.stream_to_file(speech_file_path)
    +    return speech_file_path
    +
    +# Example usage:
    +# speech_file = text_to_speech("Today is a wonderful day to build something people love!")
    ```

--- app/text_to_speech.py
+++ app/text_to_speech.py
@@ -1,6 +1,19 @@
 from pathlib import Path
 from openai import OpenAI
-client = OpenAI()
+
+def text_to_speech(text):
+    client = OpenAI()
+    speech_file_path = Path(__file__).parent / "speech.mp3"
+    response = client.audio.speech.create(
+        model="tts-1",
+        voice="alloy",
+        input=text
+    )
+    response.stream_to_file(speech_file_path)
+    return speech_file_path
+
+# Example usage:
+# speech_file = text_to_speech("Today is a wonderful day to build something people love!")
 
 speech_file_path = Path(__file__).parent / "speech.mp3"
 response = client.audio.speech.create(
