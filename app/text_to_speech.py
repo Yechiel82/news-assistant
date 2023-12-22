f1158bf788a9f053260e82e04d52cbaf2c488b03
Aleksandr Bobrov <al.bobrov.mail@gmail.com>
2023-12-22 18:17:14 +0400

    Refactor CLI usage to accept output file path as argument.

--- app/text_to_speech.py
+++ app/text_to_speech.py
@@ -16,10 +16,10 @@ def text_to_speech(text):
 
 # CLI usage
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python text_to_speech.py 'text to convert to speech'")
+    if len(sys.argv) != 3:
+        print("Usage: python text_to_speech.py 'text to convert to speech' 'output_file_path'")
         sys.exit(1)
-    text = sys.argv[1]
-    speech_file = text_to_speech(text)
+    text, file_path = sys.argv[1], sys.argv[2]
+    speech_file = text_to_speech(text, file_path)
     print(f"Speech file created at: {speech_file}")
 
