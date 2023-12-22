921d4e61e3aabf4d9d0bf4bf97b76910707e17af
Aleksandr Bobrov <al.bobrov.mail@gmail.com>
2023-12-22 18:25:09 +0400

    aider: Refactor CLI argument handling to allow for optional output file path.
    # Aider chat conversation:
    
    ASSISTANT: ```diff
    --- app/text_to_speech.py
    +++ app/text_to_speech.py
    @@ ... @@
     if __name__ == "__main__":
    -    if len(sys.argv) != 2:
    -        print("Usage: python text_to_speech.py 'text to convert to speech' 'output_file_path'")
    +    if len(sys.argv) < 2:
    +        print("Usage: python text_to_speech.py 'text to convert to speech' [output_file_path]")
             sys.exit(1)
         text = sys.argv[1]
    -    file_path = sys.argv[2]
    +    file_path = sys.argv[2] if len(sys.argv) > 2 else None
    
         speech_file = text_to_speech(text, file_path)
         print(f"Speech file created at: {speech_file}")
    ```

--- app/text_to_speech.py
+++ app/text_to_speech.py
@@ -17,11 +17,11 @@ def text_to_speech(text, speech_file_path=None):
 
 # CLI usage
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python text_to_speech.py 'text to convert to speech' 'output_file_path'")
+    if len(sys.argv) < 2:
+        print("Usage: python text_to_speech.py 'text to convert to speech' [output_file_path]")
         sys.exit(1)
     text = sys.argv[1]
-    file_path = sys.argv[2]
+    file_path = sys.argv[2] if len(sys.argv) > 2 else None
 
     speech_file = text_to_speech(text, file_path)
     print(f"Speech file created at: {speech_file}")
