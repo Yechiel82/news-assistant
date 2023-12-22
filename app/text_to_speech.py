b2c6c274b83618e3b95a9a474193ca6ae0481caf
Aleksandr Bobrov <al.bobrov.mail@gmail.com>
2023-12-22 18:15:44 +0400

    aider: Add CLI access to the function and parse text as an argument.
    # Aider chat conversation:
    
    USER: please add cli access to this function and parse text as arg
    
    ASSISTANT: ```diff
    --- app/text_to_speech.py
    +++ app/text_to_speech.py
    @@ ... @@
     from pathlib import Path
     from openai import OpenAI
    
    +import sys
    +
     def text_to_speech(text):
         client = OpenAI()
         speech_file_path = Path(__file__).parent / "speech.mp3"
    @@ ... @@
    -# Example usage:
    -# speech_file = text_to_speech("Today is a wonderful day to build something people love!")
    +# CLI usage
    +if __name__ == "__main__":
    +    if len(sys.argv) != 2:
    +        print("Usage: python text_to_speech.py 'text to convert to speech'")
    +        sys.exit(1)
    +    text = sys.argv[1]
    +    speech_file = text_to_speech(text)
    +    print(f"Speech file created at: {speech_file}")
    
    -speech_file_path = Path(__file__).parent / "speech.mp3"
    -response = client.audio.speech.create(
    -  model="tts-1",
    -  voice="alloy",
    -  input="Today is a wonderful day to build something people love!"
    -)
    -
    -response.stream_to_file(speech_file_path)
    ```

--- app/text_to_speech.py
+++ app/text_to_speech.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from openai import OpenAI
 
+import sys
+
 def text_to_speech(text):
     client = OpenAI()
     speech_file_path = Path(__file__).parent / "speech.mp3"
@@ -12,14 +14,12 @@ def text_to_speech(text):
     response.stream_to_file(speech_file_path)
     return speech_file_path
 
-# Example usage:
-# speech_file = text_to_speech("Today is a wonderful day to build something people love!")
+# CLI usage
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python text_to_speech.py 'text to convert to speech'")
+        sys.exit(1)
+    text = sys.argv[1]
+    speech_file = text_to_speech(text)
+    print(f"Speech file created at: {speech_file}")
 
-speech_file_path = Path(__file__).parent / "speech.mp3"
-response = client.audio.speech.create(
-  model="tts-1",
-  voice="alloy",
-  input="Today is a wonderful day to build something people love!"
-)
-
-response.stream_to_file(speech_file_path)
